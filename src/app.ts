917dfb528fe0c4654a869e8f28e01a1460fb5ece
s.kuehner <s.kuehner@enerparc.com>
2023-07-24 21:58:07 +0200

    calc angles ok

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import fs from "fs";
+import { getPosition } from "suncalc";
 
 async function main() {
     if (!fs.existsSync("log.txt")) {
         fs.writeFileSync("log.txt", 'time;value\r\n');
+    }
+
+    const start = new Date('2023-01-01T00:00:00.000Z');
+    const end = new Date('2023-12-31T23:00:00.000Z');
+    const hour = 1000 * 60 * 60; // in milliseconds
+
+    for (let current = start.getTime(); current <= end.getTime(); current += hour) {
+        const sunPos = getPosition(
+            new Date(current),
+            53.545, 9.936
+        );
+
+        console.log(sunPos.altitude * 180 / Math.PI);
     }
 }
 
